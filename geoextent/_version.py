3e04bc8d5a63451993bfeeaf0ce235b6c71cd78f
Daniel Nüst <daniel.nuest@tu-dresden.de>
2025-10-08 12:01:19 +0200

    bump version and tag

--- geoextent/_version.py
+++ geoextent/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.10.1.dev2+g8f0e63d75.d20251008"
-__version_tuple__ = version_tuple = (0, 10, 1, "dev2", "g8f0e63d75.d20251008")
+__version__ = version = '0.11.1.dev0+g714f2e97f.d20251008'
+__version_tuple__ = version_tuple = (0, 11, 1, 'dev0', 'g714f2e97f.d20251008')
 
-__commit_id__ = commit_id = "g8f0e63d75"
+__commit_id__ = commit_id = 'g714f2e97f'
