97791bc2d053b9adc8347fa2ddaa4982cd938a73
Build bot daniel.nuest@tu-dresden.de <daniel.nuest@tu-dresden.de>
2026-05-18 09:31:07 +0000

    Rebuild pages at eed675d68bb13c684a24dfcd1253d09e98e82e89 with branch refs/heads/main

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.13.1.dev4+g955f01267',
+    VERSION: '0.13.1.dev5+geed675d68',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
